406a8f575ffc0a80a92bfd1bcae29c4b5d533719
Teniola Fatunmbi <fatunmbiteniola001@gmail.com>
2021-08-19 21:14:40 -0600

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 
 app.get("/", async(req, res) => {
     await res.send("My name is Teniola Fatunmbi");
-    return res;
+    console.log("Teniola Fatunmbi");
 });
 
 
